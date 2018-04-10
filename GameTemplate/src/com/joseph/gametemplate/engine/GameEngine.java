1ecfba1cc7686bd29110ca33681bd331b19ac78a
Joseph <kartingman25@gmail.com>
2018-04-10 14:01:27 -0400

    Fix Eclipse's derps

--- GameTemplate/src/com/joseph/gametemplate/engine/GameEngine.java
+++ GameTemplate/src/com/joseph/gametemplate/engine/GameEngine.java
@@ -1,7 +1,6 @@
 package com.joseph.gametemplate.engine;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
